3b3d09c02edf544b68e7095d0e67a71337c7eb54
quannhmy4a <quannhm@yes4all.com>
2025-03-18 10:40:00 +0700

    na.py

--- na.py
+++ na.py
@@ -47,9 +47,7 @@ def piebytab(piebytabdf, title):
         annotations=[dict(text=f"{len(piebytabdf.index)}", 
                           x=0.5, y=0.5, 
                           font=dict(size=70, color="grey"), showarrow=False)])
-    
-    # fig.update_traces(textinfo='value')
-    
+        
     st.plotly_chart(fig)
     
 # 2. Col 1, Total Missing SKU by Field by SSO/ SFO:
@@ -87,7 +85,7 @@ def barbytab(barbytabdf):
 # Function to draw pie chart of each department (Number of Sufficient vs Missing SKU):
 def piebydept(dept, piebydeptdf):
     
-    custom_colors = ['#e62023', '#1abd3b']
+    custom_colors = ['#c40202', '#1abd3b']
     
     fig = go.Figure(data=[go.Pie(
         labels=piebydeptdf[dept].drop('Total by Dept').index,  # dept names
@@ -125,48 +123,50 @@ def barbydept(dept, barbydeptdf):
     textfont_size=12,
     textposition="outside",  # Show text labels above bars
     marker=dict(
-        color='#fff900',
+        color='#c40202',
         line=dict(width=1, color="#e62003"),  # Add border
         opacity=0.9,  # Slight transparency
         cornerradius=60),)
-    fig.update_layout(xaxis=dict(range=[0, max(missbyfield[dept]) * 1.5]))  # Adjust margins as needed
+    fig.update_layout(title_text='Missing Field Break- down',
+        xaxis=dict(range=[0, max(missbyfield[dept]) * 1.5]))  # Adjust margins as needed
 
-    # width=0.1 if len(missbyfield) == 1 else None)
     st.plotly_chart(fig, use_container_width=True)
 
 # Function to display dataframe of each department (group SKU by the same group of Missing Field)
-def detailbysku(dept, detailbyskudf):
-    # Data- prep:
-    detailbyskudf.set_index('SKU', inplace=True)
-    missing_patterns = detailbyskudf[detailbyskudf['dept'] == dept].isna().apply(lambda row: tuple(row[row].index), axis=1)
-    grouped_skus = missing_patterns.groupby(missing_patterns).apply(lambda x: list(x.index)).to_dict()
+def detailbysku(dept, detailbyskudf, key):    
+    detailbyskudf.set_index(key, inplace=True)
+    missing_patterns = detailbyskudf[detailbyskudf['dept'] == 'SSO'].isna().apply(lambda row: tuple(row[row].index), axis=1)
+    def group_by_pic(df, missing_patterns):
+        grouped_data = {}
+        for (sku, missing_cols), pic in zip(missing_patterns.items(), detailbyskudf.loc[missing_patterns.index, 'PIC']):
+            grouped_data.setdefault((pic, missing_cols), []).append(sku)
+        return grouped_data
+    grouped_skus = group_by_pic(detailbyskudf, missing_patterns)
     output_df = pd.DataFrame(
-        [(', '.join(missing_cols), ', '.join(skus)) for missing_cols, skus in grouped_skus.items()],
-        columns=["Missing Columns", "SKUs"])
-    output_df["Missing Count"] = output_df["Missing Columns"].apply(lambda x: len(x.split(", ")) if x else 0)    
-    output_df = output_df.sort_values(by="Missing Count", ascending=False).drop(columns=["Missing Count"])
-    output_df.reset_index(drop=True, inplace=True)    
-    # missing_col_options = sorted(output_df["Missing Columns"].unique(), key=lambda x: len(x.split(", ")) if x else 0, reverse=True)
-    
+        [(pic, ', '.join(missing_cols), ', '.join(skus)) for (pic, missing_cols), skus in grouped_skus.items() if missing_cols],
+        columns=["PIC", "Missing Columns", "SKUs"])
+    output_df["Missing Count"] = output_df["Missing Columns"].apply(lambda x: len(x.split(", ")) if x else 0)
+    output_df = output_df.sort_values(by=["PIC", "Missing Count"], ascending=[True, False]).drop(columns=["Missing Count"])
+    output_df.set_index('PIC', inplace=True)    
     with st.expander("Group of Missing"):
         st.dataframe(output_df, use_container_width = True)
 
 # Function to display dataframe of each department (group SKU by Missing Field and PIC)
 def filterfield(dept, filferfielddf, filferfieldmask, key):
+    st.subheader("Detail Missing Field by Team")
     outfildf = filferfielddf[filferfieldmask]
     selected_list = outfildf[outfildf['dept'] == dept].columns[outfildf.isna().any()].tolist()
-    selected_column = st.selectbox("Select a column", selected_list, key=f"select_{dept}")
+    selected_column = st.selectbox("Select Field to view Missing SKU by PIC", selected_list, key=f"select_{dept}")
     filtered_df = outfildf[outfildf['dept'] == dept][['PIC', key, selected_column]]                     
     filtered_df[selected_column] = filtered_df[selected_column].isna()
     filtered_df = filtered_df[filtered_df[selected_column]]
-
     # Group by PIC and count blank SKUs and total SKUs
     summary_df = filtered_df.groupby("PIC").agg(
-        blank_SKU=(key, "count"),
+        MissingCount=(key, "count"),
         SKU=(key, lambda x: ", ".join(x))
     ).reset_index()
-    summary_df = summary_df.sort_values(by='blank_SKU', ascending=False)
-    st.write("Summary of Blank Values in", selected_column)
+    summary_df = summary_df.sort_values(by='MissingCount', ascending=False)
+    summary_df.set_index('PIC', inplace=True)
     st.dataframe(summary_df, use_container_width = True)
 
     # Streamlit UI
@@ -276,7 +276,7 @@ while True:
                 with col2:
                     barbydept('SSO', out_df)            
                 filterfield('SSO', mandf, mask, 'SKU')
-                detailbysku('SSO', mandf[mask])
+                detailbysku('SSO', mandf[mask], 'SKU')
             with sub_tab2: #SFO
                 col1, col2 = st.columns(2)
                 with col1:
@@ -284,7 +284,7 @@ while True:
                 with col2:
                     barbydept('SFO', out_df)
                 filterfield('SFO', mandf, mask, 'SKU')
-                detailbysku('SFO', mandf[mask])                
+                detailbysku('SFO', mandf[mask], 'SKU')                
     
     with proven_tab: # Product Data by Vendor =============================
         st.markdown("<h1 style='font-size: 20px;'>Overview</h1>", unsafe_allow_html=True)
@@ -306,7 +306,7 @@ while True:
                 with col2:
                     barbydept('SSO', out_dfven)
                 filterfield('SSOven', mandfvenfilterfield, mandfvenfilterfield.isna().any(axis=1), 'SKU_SupCode')
-                detailbysku('SSO', mandfven[mandfven.isna().any(axis=1)])        
+                detailbysku('SSO', mandfven[mandfven.isna().any(axis=1)], 'SKU_SupCode')        
             with sub_tab2: #SFO
                 col1, col2 = st.columns(2)
                 with col1:
@@ -314,7 +314,7 @@ while True:
                 with col2:
                     barbydept('SFO', out_dfven)
                 filterfield('SFOven', mandfvenfilterfield, mandfvenfilterfield.isna().any(axis=1), 'SKU_SupCode')
-                detailbysku('SFO', mandfven[mandfven.isna().any(axis=1)])
+                detailbysku('SFO', mandfven[mandfven.isna().any(axis=1)], 'SKU_SupCode')
  
     # Auto-refresh every setting seconds =======================================
     auto_refresh(3)
